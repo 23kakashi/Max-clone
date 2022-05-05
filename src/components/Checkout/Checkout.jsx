81c06ca7da9cba86e397110de91ac726f05f1b77
Krishan Kumar Safi <99599853+Krishukr12@users.noreply.github.com>
2022-05-05 22:21:15 +0530

    first commit

---
+++ src/components/Checkout/Checkout.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import CheckoutHeader from './CheckoutHeader'
+import ShippingMethod from './ShippingMethod'
+const Checkout = () => {
+  return (
+
+      <div>
+        <div>  <CheckoutHeader />
+      </div>
+      <div>
+        <ShippingMethod />
+      </div>
+    </div>
+
+
+
+  )
+}
+
+export default Checkout
--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -3,17 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "../components/navbar/Navbar"
 import Footer from "../components/footer/Footer";
 import Home from "../pages/Home";
-
+import Checkout from "../components/Checkout/Checkout";
 const MainRoutes = () => {
   return (
     <div>
-      <Navbar />
+           {/* <Navbar />
       
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
 
-      <Footer />
+      <Footer /> */}
+      <Checkout/>
     </div>
   );
 };
