c926cc5a69de875d99589ba0f52ab5e63dfdf030
AdityaArnav <99977930+AdityaArnav@users.noreply.github.com>
2022-05-05 11:34:45 +0530

    fetched data and removed bugs

--- src/components/ProductPage/Women.jsx
+++ src/components/ProductPage/Women.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
-import data from "./data.json";
+import React, { useEffect, useState } from 'react'
+// import data from "./data.json";
 import style from "./women.module.css"
 
 const Women = () => {
-    console.log(data);
+    // console.log(data);
+const [show, setShow]=useState([])
 
+    useEffect(()=>{
+
+        fetch(`http://localhost:8080/dresses`)
+        .then((res)=>res.json())
+        .then((data)=>setShow(data))
+        .catch((er)=>console.log(er))
+    },[])
+
+console.log(show);
 
     return (
         <>
             <h1 className={style.TopHead}>Tops</h1>
 
             <div className={style.main}>
-                {data.map((item) =>
+                {show.map((item) =>
 
                     <div className={style.card} key={item.id}>
 
                         <div className={style.card_img}>
-                            <img src={item.image_url} />
+                            <img src={item.image_url} alt='images' />
                         </div>
                         <div className={style.card_header}>
                             <h3 className={style.price}>{"₹"+item.price}</h3>
