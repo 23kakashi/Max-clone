079ca6a3092ef52bd9bf2fc292b020aeae17f023
Adityaprajapati26 <99543597+Adityaprajapati26@users.noreply.github.com>
2022-05-06 12:47:42 +0530

    Sigin page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import "./App.css";
-import MainRoutes from "./routes/MainRoutes";
+// import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router';
+import './App.css';
+import Navbar from './components/Navbar';
+import Sign from './components/Sign';
+
+
+
 function App() {
   return (
     <div className="App">
-      <MainRoutes />
+      <Navbar/>
+      <Routes>
+        <Route path="signin/*" element={<Sign/>}/>
+        
+      </Routes>
+    
     </div>
   );
 }
+
 export default App;
