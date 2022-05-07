4cb07fe2c4c8dea71797d1a8697cc0d3682c0ee8
Balram Patidar <pbalram088@gmail.com>
2022-05-07 20:21:15 +0530

    Update MainRoutes.jsx

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+<<<<<<< Updated upstream
 import Navbar from "../components/navbar/Navbar";
+=======
+// import Navbar from "../components/navbar/Navbar"
+ import NavbarSecond from "../components/navbar/NavbarSecond";
+ import NavbarTop from "../components/navbar/NavbarTop";
+>>>>>>> Stashed changes
 import Footer from "../components/footer/Footer";
 import Home from "../pages/Home";
 import About from "../pages/About";
@@ -9,8 +15,14 @@ import Women from "../components/ProductPage/Women/Women";
 const MainRoutes = () => {
   return (
     <div>
+<<<<<<< Updated upstream
       <Navbar />
 
+=======
+      <NavbarTop />
+      <NavbarSecond/>
+      
+>>>>>>> Stashed changes
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
