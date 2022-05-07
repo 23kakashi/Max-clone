b2ef61f225c709b8032b03ae4914edb84a6f99d9
Balram Patidar <pbalram088@gmail.com>
2022-05-07 20:20:52 +0530

    navbars
    navbar

---
+++ src/components/navbar/NavbarTop.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import StorefrontOutlinedIcon from '@mui/icons-material/StorefrontOutlined';
+
+
+const Wrapper = styled.div`
+
+display: flex;
+width:100%;
+gap:100px;
+font-weight:500;
+font-size:14px;
+padding:5px;
+color:white;
+margin:auto;
+overflow:hidden;
+position:fixed;
+
+background-color:black;
+
+
+a{
+  text-decoration: none;
+}
+.left {
+    color:white;
+    display:flex;
+    gap:30px;
+    width:50%;
+    justify-content:space-aroun;
+    
+}
+.right {
+    color:white;
+    display:flex;
+    gap:30px;
+    width:50%;
+    justify-content:right;
+    
+}
+abbr{
+    text-decoration: none;
+}
+`
+
+export const NavbarTop = () => {
+  // const { token } = useContext(AuthContext);
+  // use token to chnage the text from Login to Logout once logged in successfully
+ 
+  return (
+    <>
+       {/* keep all the NavLinks here */}
+     <Wrapper>
+     <div class="left">
+            <div> <LocalShippingIcon/>  <Link to="/free-shipping"> Free Shipping </Link>  </div>
+            <div> <StorefrontOutlinedIcon/>  <Link to="/return-to-store">Return To Store </Link>  </div>
+            <div> <StorefrontOutlinedIcon/>  <Link to="/click-and-collect"> Click & Collect </Link>  </div>
+           
+     </div>
+     <div class="right">
+            <div>  <Link to="/download-our-app">DownLoad Our Apps</Link>  </div>
+            <div>  <Link to="/store-locator">Store Locator</Link>  </div>
+            <div>  <Link to="/help">Help</Link>  </div>
+     </div>
+</Wrapper>
+    
+    </>
+
+  )
+}
+
+export default NavbarTop
