6cb537fdcce04f2ff3c0384745951622f6a6c56c
Krishan Kumar Safi <99599853+Krishukr12@users.noreply.github.com>
2022-05-08 17:37:01 +0530

    final commit

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import CheckoutHeader from './CheckoutHeader.jsx'
 import ShippingMethod from './ShippingMethod.jsx'
 import Payment_method from './Payment_method.jsx'
+import Payment_other from './Payment_other.jsx'
 const Checkout = () => {
   return (
 
@@ -14,6 +15,9 @@ const Checkout = () => {
       </div>
       <div>
        <Payment_method/>
+      </div>
+      <div>
+    <Payment_other/>
       </div>
     </div>
 
---
+++ src/components/Checkout/Payment_other.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import classes from "./Checkout.module.css"
+const Payment_other = () => {
+    return (
+        <div className={classes.Payment_other}>
+            <div className={classes.Payment_other2}>
+                <div>
+                    <img style={{ width: "50px" }} src='https://i1.lmsin.net/website_images/in/my-account/ico-payment-netbanking.png'></img>
+                </div>
+                <div>
+                    <strong >Net Banking</strong><br></br>
+                    <span>Pay by your preferred bank account.</span>
+                </div>
+            </div>
+            <div className={classes.Payment_other2}>
+            <div>
+           <img style={{width:"50px"}} src='https://i1.lmsin.net/website_images/in/my-account/ico-payment-wallet.png'></img>
+           </div>
+           <div>
+           <strong >Wallets</strong><br></br>
+            <span>Pay with your preferred wallet.</span>
+           </div>
+            </div>
+            <div className={classes.Payment_other2}>
+            <div>
+           <img style={{width:"50px"}} src='https://dd311b21463486cea6e6-ba57f67b7c05f7cbe632f35f74bd7340.ssl.cf3.rackcdn.com/Checkout/order_summary.png'></img>
+           </div>
+           <div>
+           <strong >Cash On Delivery</strong><br></br>
+            <span>+ Rs49</span>
+           </div>
+            </div>
+            <div className={classes.Payment_other2}>
+            <div>
+           <img style={{width:"50px"}} src='https://i1.lmsin.net/website_images/payment/upi.png'></img>
+           </div>
+           <div>
+           <strong >Cash On Delivery</strong><br></br>
+            <span>+ Rs49</span>
+           </div>
+            </div>
+        </div>
+    )
+}
+
+export default Payment_other
