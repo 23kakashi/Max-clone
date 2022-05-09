2cfc4ae5fb949a5380ad1fe2dc89b37a72a78a47
Krishan Kumar Safi <99599853+Krishukr12@users.noreply.github.com>
2022-05-09 12:59:27 +0530

    final commit

--- src/components/Checkout/Ways.jsx
+++ src/components/Checkout/Ways.jsx
@@ -3,7 +3,7 @@ import classes from "./Checkout.module.css"
 const Ways = () => {
   return (
     <div  className={classes.ways}>
-        <h1>Ways You can Pay</h1><br></br>
+        <h1 style={{fontWeight:"bold",marginTop:"20px"}}>Ways You can Pay</h1><br></br>
        <div className={classes.ways2}>
        <div>
             <img style={{height:"40px",width:"60px"}} src='https://i1.lmsin.net/website_images/ae/checkout/logo-visa.png'></img>
